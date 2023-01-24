e23880e5e8356e8ff51ec5710b8ae34c2dfd6c88
jacksonlevine <jack@netbug.us>
2023-01-24 15:46:49 -0800

    add form submit handler and event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,32 @@ Ticket.prototype.selectedTicket = function() {
   return this.movieName + " " + this.movieTime + " " + this.special;
 }
 
+Ticket.prototype.calculateCost = function() {
+   let cost = 10;
+    if ( this.special === "matinee") {
+        return cost += 5.00;
+    }else if(this.special === "youth") {
+        return cost += 7.00;
+    }else if (this.special === "senior") {
+        return cost += 4.00;
+    }
+}
 
 // UI Logic -------
+
+function handleFormSubmission(event){
+    event.preventDefault();
+    const selectedMovieName = document.querySelector("select#movie-name").value; // alt document.getElementById("movie-name").value;
+    const selectedMovieTime = document.querySelector("select#movie-time").value;
+    const selectedSpecial = document.querySelector("select#special").value;
+    let newTicket = new Ticket(selectedMovieName, selectedMovieTime, selectedSpecial);
+    console.log(newTicket.selectedTicket());
+    console.log(newTicket.calculateCost());
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("movie-tickets").addEventListener("submit", handleFormSubmission);
+});
 
 // function displayTicketDetails(event) {
 //   const ticket = 
